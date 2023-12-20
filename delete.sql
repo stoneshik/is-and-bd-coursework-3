ab27f269d310a65f28b280e5a9e54f8eebe57bcc
stoneshik <strebilly@mail.ru>
2023-12-20 16:07:41 +0300

    фикс

---
+++ delete.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS
+    -- Удаление таблиц ассоциативных сущностей
+    role_authorities,
+    user_roles,
+    function_variants,
+    scan_task_files,
+    print_task_files,
+    machine_files,
+    -- Характеристические сущности
+    replenishes,
+
+    ;
