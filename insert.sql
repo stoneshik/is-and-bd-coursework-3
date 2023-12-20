20ddb680bd27570856b733fe7ce7177ba4346217
stoneshik <strebilly@mail.ru>
2023-12-20 22:01:15 +0300

    добавил вставку тестовых данных о вендинговых точках и вендинговых аппаратов

--- insert.sql
+++ insert.sql
@@ -66,3 +66,110 @@ VALUES
     (4, 2, 70, default)
     ;
 -- Вендинговые точки
+INSERT INTO vending_points(
+    vending_point_id,
+    vending_point_address,
+    vending_point_description,
+    vending_point_number_machines,
+    vending_point_cords)
+VALUES
+    (0,
+     'Невский пр-кт, 2',
+     'Находится рядом с банкоматом',
+     2,
+     point(59.93716299256363, 30.313378213455664)),
+    (1,
+     'Комендансткий пр-кт, 34',
+     'Находится в подвале рядом с шаурмечной',
+     1,
+     point(60.02097852901829, 30.242859111340625))
+    ;
+INSERT INTO vending_point_schedules(
+    vending_point_schedule_id,
+    vending_point_id,
+    vending_point_schedule_day_week,
+    vending_point_schedule_time_start,
+    vending_point_schedule_time_end)
+VALUES
+    (0, 0, 'monday', '10:00', '20:00'),
+    (1, 0, 'tuesday', '09:00', '21:00'),
+    (2, 0, 'wednesday', '11:00', '23:00'),
+    (3, 0, 'thursday', '10:00', '20:00'),
+    (4, 0, 'friday', '10:00', '20:00'),
+    (5, 1, 'saturday', '06:35', '22:20'),
+    (6, 1, 'sunday', '09:03', '21:55')
+    ;
+INSERT INTO vending_point_unusual_schedules(
+    vending_point_unusual_schedule_id,
+    vending_point_id,
+    vending_point_unusual_schedule_date,
+    vending_point_schedule_time_start,
+    vending_point_schedule_time_end)
+VALUES
+    (0, 0, '15.11.2024', '02:00', '22:00')
+    ;
+-- Вендинговые аппараты (машины)
+INSERT INTO machines(machine_id, vending_point_id)
+VALUES
+    (0, 0),
+    (1, 0),
+    (2, 1)
+    ;
+INSERT INTO function_variants(function_variant_id, vending_point_id, machine_id, function_variant)
+VALUES
+    (0, 0, 0, 'black_white_print'),
+    (1, 0, 0, 'color_print'),
+    (2, 0, 1, 'black_white_print'),
+    (3, 0, 1, 'color_print'),
+    (4, 0, 1, 'scan'),
+    (5, 1, 2, 'black_white_print'),
+    (5, 1, 2, 'scan')
+    ;
+INSERT INTO machine_supplies(
+    machine_supplies_id,
+    machine_id,
+    machine_supplies_quantity_paper,
+    machine_supplies_ink_level,
+    machine_supplies_datetime)
+VALUES
+    (0, 0, 100, 900, default),
+    (1, 0, 40, 800, default),
+    (2, 1, 200, 500, default),
+    (3, 2, 800, 1000, default)
+    ;
+INSERT INTO machine_conditions(machine_condition_id, machine_id, machine_status, machine_condition_datetime)
+VALUES
+    (0, 0, 'work', default),
+    (1, 0, 'temporarily_not_work', default),
+    (2, 0, 'work', default),
+    (3, 1, 'work', default),
+    (0, 2, 'temporarily_not_work', default)
+    ;
+-- Заказы/задания
+INSERT INTO orders(
+    order_id,
+    account_id,
+    vending_point_id,
+    order_amount,
+    order_datetime,
+    order_type,
+    order_status,
+    order_num)
+VALUES
+    (),
+    ;
+INSERT INTO scan_tasks(scan_task_id, order_id, machine_id, scan_task_number_pages)
+VALUES
+    (),
+    ;
+INSERT INTO print_tasks(
+    print_task_id,
+    order_id,
+    machine_id,
+    print_task_color,
+    print_task_is_entire_document,
+    print_task_selected_pages,
+    print_task_number_copies)
+VALUES
+    (),
+    ;
