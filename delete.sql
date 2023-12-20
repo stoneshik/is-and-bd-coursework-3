f3b8b05df92a1f6949a1276415a117fce42a05e7
stoneshik <strebilly@mail.ru>
2023-12-20 16:19:31 +0300

    скрипт удаления таблиц

--- delete.sql
+++ delete.sql
@@ -6,7 +6,20 @@ DROP TABLE IF EXISTS
     scan_task_files,
     print_task_files,
     machine_files,
-    -- Характеристические сущности
+    -- Осталььные сущности
+    scan_tasks,
+    print_tasks,
+    machine_supplies,
+    machine_conditions,
+    machines,
+    vending_point_schedules,
+    vending_point_unusual_schedules,
+    vending_points,
+    orders,
     replenishes,
-
+    accounts,
+    files,
+    users,
+    roles,
+    authorities
     ;
